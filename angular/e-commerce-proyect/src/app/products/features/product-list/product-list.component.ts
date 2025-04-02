f2cabfd81a212668516c504c50547167596ae799
GaRIEN <gabrielhuaman683@gmail.com>
2025-04-02 10:22:05 -0500

    creacion del proyecto e-commerce

---
+++ angular/e-commerce-proyect/src/app/products/features/product-list/product-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-list',
+  imports: [],
+  templateUrl: './product-list.component.html',
+  styles: ``,
+})
+export default class ProductListComponent {}
