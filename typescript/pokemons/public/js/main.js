5bcd2aeecb85588ff3e58e0945148b4ab8c9fa81
GaRIEN <gabrielhuaman683@gmail.com>
2025-04-14 21:25:45 -0500

    cambios en refactorizar el  codigo

--- typescript/pokemons/public/js/main.js
+++ typescript/pokemons/public/js/main.js
@@ -1,3 +1,12 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 import { helpHttp } from "./helpHttp.js";
 document.addEventListener("DOMContentLoaded", renderPokemons);
 //constantes
@@ -9,59 +18,132 @@ const loader = document.getElementById("loader");
 const toggleLoader = (show) => {
     loader.style.display = show ? "block" : "none";
 };
+// function renderPokemons() {
+//   toggleLoader(true);
+//   api.get(API_URL).then((res: listPokemon | httpResponseError) => {
+//     if ("err" in res) {
+//       console.log("Error: ", res);
+//     } else {
+//       let pokemonsHTML = "";
+//       res.results.forEach((pokemon) => {
+//         api.get(pokemon.url).then((res: any) => {
+//           if ("err" in res) {
+//             console.log("Error: ", res);
+//           } else {
+//             console.log("res", res);
+//             const types = res.types.map((type: any) => type.type.name);
+//           }
+//         });
+//         pokemonsHTML += `
+//             <div class="flip-card mx-auto">
+//               <div class="flip__card-inner">
+//                 <div
+//                   class="flip__card-front d-flex flex-column rounded shadow border-0 bg-light"
+//                 >
+//                   <img
+//                     src="./assets/img/clientegalvan.jpeg"
+//                     class="img-fluid rounded"
+//                     style="width: 100%; height: 70%"
+//                     alt="Avatar"
+//                   />
+//                   <h5 class="fs-5 fw-medium m-2">${pokemon.name}</h5>
+//                   <div class="d-flex m-2 gap-3">
+//                     <button
+//                       class="bg-danger text-white fw-bold border-0 shadow rounded my-1"
+//                       style="font-size: 0.7rem"
+//                     >
+//                       <span>habilidad</span>
+//                     </button>
+//                     <button
+//                       class="bg-danger text-white fw-bold border-0 shadow rounded my-1"
+//                       style="font-size: 0.7rem"
+//                     >
+//                       <span>habilidad</span>
+//                     </button>
+//                   </div>
+//                 </div>
+//                 <div
+//                   class="flip__card-back d-flex flex-colmn align-items-center justify-content-center rounded shadow bg-primary text-white"
+//                 >
+//                   <h1>hola reverso</h1>
+//                   <p>Architect & Engineer</p>
+//                   <p>We love that guy</p>
+//                 </div>
+//               </div>
+//             </div>
+//     `;
+//       });
+//       toggleLoader(false);
+//       $pokemons.innerHTML = pokemonsHTML;
+//     }
+//   });
+// }
 function renderPokemons() {
-    toggleLoader(true);
-    const pokemons = api
-        .get(API_URL)
-        .then((res) => {
-        if ("err" in res) {
-            console.log("Error: ", res);
-        }
-        else {
-            let pokemonsHTML = "";
-            res.results.forEach((pokemon) => {
-                pokemonsHTML += `
-            <div class="flip-card mx-auto">
-              <div class="flip__card-inner">
-                <div
-                  class="flip__card-front d-flex flex-column rounded shadow border-0 bg-light"
-                >
-                  <img
-                    src="./assets/img/clientegalvan.jpeg"
-                    class="img-fluid rounded"
-                    style="width: 100%; height: 70%"
-                    alt="Avatar"
-                  />
-
-                  <h5 class="fs-5 fw-medium m-2">${pokemon.name}</h5>
-                  <div class="d-flex m-2 gap-3">
-                    <button
-                      class="bg-danger text-white fw-bold border-0 shadow rounded my-1"
-                      style="font-size: 0.7rem"
-                    >
-                      <span>habilidad</span>
-                    </button>
-                    <button
-                      class="bg-danger text-white fw-bold border-0 shadow rounded my-1"
-                      style="font-size: 0.7rem"
-                    >
-                      <span>habilidad</span>
-                    </button>
-                  </div>
-                </div>
-                <div
-                  class="flip__card-back d-flex flex-colmn align-items-center justify-content-center rounded shadow bg-primary text-white"
-                >
-                  <h1>hola reverso</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
-                </div>
-              </div>
-            </div>
-    `;
-            });
-            toggleLoader(false);
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            toggleLoader(true);
+            const res = yield api.get(API_URL);
+            if ("err" in res) {
+                console.error("Error al obtener la lista de pokemons:", res);
+                return;
+            }
+            const pokemonDetails = yield fetchAllPokemonDetails(res.results);
+            console.log("pokemons details");
+            console.log(pokemonDetails);
+            const pokemonsHTML = pokemonDetails
+                .map((pokemon) => renderPokemonCard(pokemon))
+                .join("");
             $pokemons.innerHTML = pokemonsHTML;
         }
+        catch (error) {
+            console.error("Error inesperado:", error);
+        }
+        finally {
+            toggleLoader(false);
+        }
     });
+}
+function fetchAllPokemonDetails(pokemons) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const promises = pokemons.map((pokemon) => api.get(pokemon.url).then((res) => {
+            if ("err" in res) {
+                console.error("Error al obtener detalle de pokemon:", res);
+                return null;
+            }
+            return res;
+        }));
+        const results = yield Promise.all(promises);
+        return results.filter((pokemon) => pokemon !== null);
+    });
+}
+function renderPokemonCard(pokemon) {
+    const types = pokemon.types.map((type) => type.type.name).join(", ");
+    return `
+    <div class="flip-card mx-auto">
+      <div class="flip__card-inner">
+        <div class="flip__card-front d-flex flex-column rounded shadow border-0 bg-light">
+          <img src="./assets/img/clientegalvan.jpeg"
+               class="img-fluid rounded"
+               style="width: 100%; height: 70%"
+               alt="Avatar" />
+          <h5 class="fs-5 fw-medium m-2">${pokemon.name}</h5>
+          <div class="d-flex m-2 gap-3">
+            ${types
+        .split(", ")
+        .map((type) => `
+              <button class="bg-danger text-white fw-bold border-0 shadow rounded my-1" style="font-size: 0.7rem">
+                <span>${type}</span>
+              </button>
+            `)
+        .join("")}
+          </div>
+        </div>
+        <div class="flip__card-back d-flex flex-column align-items-center justify-content-center rounded shadow bg-primary text-white">
+          <h1>Hola reverso</h1>
+          <p>Architect & Engineer</p>
+          <p>We love that guy</p>
+        </div>
+      </div>
+    </div>
+  `;
 }
--- typescript/pokemons/src/ts/main.ts
+++ typescript/pokemons/src/ts/main.ts
@@ -17,58 +17,137 @@ const toggleLoader = (show: boolean): void => {
   loader.style.display = show ? "block" : "none";
 };
 
-function renderPokemons() {
-  toggleLoader(true);
-  const pokemons = api
-    .get(API_URL)
-    .then((res: listPokemon | httpResponseError) => {
-      if ("err" in res) {
-        console.log("Error: ", res);
-      } else {
-        let pokemonsHTML = "";
-        res.results.forEach((pokemon) => {
-          pokemonsHTML += `
-            <div class="flip-card mx-auto">
-              <div class="flip__card-inner">
-                <div
-                  class="flip__card-front d-flex flex-column rounded shadow border-0 bg-light"
-                >
-                  <img
-                    src="./assets/img/clientegalvan.jpeg"
-                    class="img-fluid rounded"
-                    style="width: 100%; height: 70%"
-                    alt="Avatar"
-                  />
+// function renderPokemons() {
+//   toggleLoader(true);
+//   api.get(API_URL).then((res: listPokemon | httpResponseError) => {
+//     if ("err" in res) {
+//       console.log("Error: ", res);
+//     } else {
+//       let pokemonsHTML = "";
+//       res.results.forEach((pokemon) => {
+//         api.get(pokemon.url).then((res: any) => {
+//           if ("err" in res) {
+//             console.log("Error: ", res);
+//           } else {
+//             console.log("res", res);
+//             const types = res.types.map((type: any) => type.type.name);
+//           }
+//         });
+//         pokemonsHTML += `
+//             <div class="flip-card mx-auto">
+//               <div class="flip__card-inner">
+//                 <div
+//                   class="flip__card-front d-flex flex-column rounded shadow border-0 bg-light"
+//                 >
+//                   <img
+//                     src="./assets/img/clientegalvan.jpeg"
+//                     class="img-fluid rounded"
+//                     style="width: 100%; height: 70%"
+//                     alt="Avatar"
+//                   />
 
-                  <h5 class="fs-5 fw-medium m-2">${pokemon.name}</h5>
-                  <div class="d-flex m-2 gap-3">
-                    <button
-                      class="bg-danger text-white fw-bold border-0 shadow rounded my-1"
-                      style="font-size: 0.7rem"
-                    >
-                      <span>habilidad</span>
-                    </button>
-                    <button
-                      class="bg-danger text-white fw-bold border-0 shadow rounded my-1"
-                      style="font-size: 0.7rem"
-                    >
-                      <span>habilidad</span>
-                    </button>
-                  </div>
-                </div>
-                <div
-                  class="flip__card-back d-flex flex-colmn align-items-center justify-content-center rounded shadow bg-primary text-white"
-                >
-                  <h1>hola reverso</h1>
-                  <p>Architect & Engineer</p>
-                  <p>We love that guy</p>
-                </div>
-              </div>
-            </div>
-    `;
-        });
-        toggleLoader(false);
-        $pokemons.innerHTML = pokemonsHTML;
+//                   <h5 class="fs-5 fw-medium m-2">${pokemon.name}</h5>
+//                   <div class="d-flex m-2 gap-3">
+//                     <button
+//                       class="bg-danger text-white fw-bold border-0 shadow rounded my-1"
+//                       style="font-size: 0.7rem"
+//                     >
+//                       <span>habilidad</span>
+//                     </button>
+//                     <button
+//                       class="bg-danger text-white fw-bold border-0 shadow rounded my-1"
+//                       style="font-size: 0.7rem"
+//                     >
+//                       <span>habilidad</span>
+//                     </button>
+//                   </div>
+//                 </div>
+//                 <div
+//                   class="flip__card-back d-flex flex-colmn align-items-center justify-content-center rounded shadow bg-primary text-white"
+//                 >
+//                   <h1>hola reverso</h1>
+//                   <p>Architect & Engineer</p>
+//                   <p>We love that guy</p>
+//                 </div>
+//               </div>
+//             </div>
+//     `;
+//       });
+//       toggleLoader(false);
+//       $pokemons.innerHTML = pokemonsHTML;
+//     }
+//   });
+// }
+
+async function renderPokemons() {
+  try {
+    toggleLoader(true);
+
+    const res: httpResponseError | listPokemon = await api.get(API_URL);
+    if ("err" in res) {
+      console.error("Error al obtener la lista de pokemons:", res);
+      return;
+    }
+
+    const pokemonDetails = await fetchAllPokemonDetails(res.results);
+    console.log("pokemons details");
+    console.log(pokemonDetails);
+    const pokemonsHTML = pokemonDetails
+      .map((pokemon) => renderPokemonCard(pokemon))
+      .join("");
+
+    $pokemons.innerHTML = pokemonsHTML;
+  } catch (error) {
+    console.error("Error inesperado:", error);
+  } finally {
+    toggleLoader(false);
+  }
+}
+
+async function fetchAllPokemonDetails(pokemons: any[]): Promise<any[]> {
+  const promises = pokemons.map((pokemon) =>
+    api.get(pokemon.url).then((res) => {
+      if ("err" in res) {
+        console.error("Error al obtener detalle de pokemon:", res);
+        return null;
       }
-    });
+      return res;
+    })
+  );
+  const results = await Promise.all(promises);
+  return results.filter((pokemon) => pokemon !== null);
+}
+
+function renderPokemonCard(pokemon: any): string {
+  const types = pokemon.types.map((type: any) => type.type.name).join(", ");
+  return `
+    <div class="flip-card mx-auto">
+      <div class="flip__card-inner">
+        <div class="flip__card-front d-flex flex-column rounded shadow border-0 bg-light">
+          <img src="./assets/img/clientegalvan.jpeg"
+               class="img-fluid rounded"
+               style="width: 100%; height: 70%"
+               alt="Avatar" />
+          <h5 class="fs-5 fw-medium m-2">${pokemon.name}</h5>
+          <div class="d-flex m-2 gap-3">
+            ${types
+              .split(", ")
+              .map(
+                (type: any) => `
+              <button class="bg-danger text-white fw-bold border-0 shadow rounded my-1" style="font-size: 0.7rem">
+                <span>${type}</span>
+              </button>
+            `
+              )
+              .join("")}
+          </div>
+        </div>
+        <div class="flip__card-back d-flex flex-column align-items-center justify-content-center rounded shadow bg-primary text-white">
+          <h1>Hola reverso</h1>
+          <p>Architect & Engineer</p>
+          <p>We love that guy</p>
+        </div>
+      </div>
+    </div>
+  `;
 }
